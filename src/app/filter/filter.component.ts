8860a9cf8001900bbe2ab075652f7fcd737fdb0b
prabukdatasirpi <prabuk@datasirpi.com>
2022-12-09 19:50:21 +0530

    commit

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -8,35 +8,44 @@ import { Component, NgModule, } from '@angular/core';
 export class FilterComponent {
   searchText: any;
   heroes = [
-    { id: 1, name: 'prabu', country: 'India' },
-    { id: 2, name: 'ram', country: 'USA' },
-    { id: 3, name: 'Siddhik', country: 'India' },
-    { id: 4, name: 'kannan', country: 'India' },
-    { id: 5, name: 'Sathya', country: 'UK' },
-    { id: 6, name: 'Muthu', country: 'India' },
-    { id: 7, name: 'Shathik', country: 'India' },
-    { id: 8, name: 'Kumar', country: 'India' }
+    { id: 1, name: 'prabu', country: 'india' },
+    { id: 2, name: 'ram', country: 'usa' },
+    { id: 3, name: 'siddhik', country: 'india' },
+    { id: 4, name: 'kannan', country: 'usa' },
+    { id: 5, name: 'sathya', country: 'uk' },
+    { id: 6, name: 'muthu', country: 'india' },
+    { id: 7, name: 'shathik', country: 'uk' },
+    { id: 8, name: 'kumar', country: 'uk' },
+    { id: 9, name: 'kumar', country: 'india' },
+    { id: 10, name: 'kumar', country: 'usa' }
+
 
   ];
   result = this.heroes
   constructor() { }
   test(val) {
     console.log(val);
-    var searchVal;
-
+    var  i=0;
+    console.log(i);
     if (val.target.value) {
-      this.result = this.heroes.filter(item => {
+      this.result = this.heroes.filter(item  => {
+        console.log("sathya"+i);
         if (item.id == val.target.value) {
-          searchVal = item;
-          return searchVal;
-        } if (item.name == val.target.value) {
-          searchVal = item;
-          return searchVal;
-        } if (item.country == val.target.value) {
-          searchVal = item;
-          return searchVal;
+          console.log(item.id);
+         // searchVal = item;
+          return item;
+        } 
+        if (item.name == val.target.value) {
+          console.log(item.name);
+         // searchVal = item;
+          return item;
+        } 
+        if (item.country == val.target.value) {
+          console.log(item.country);
+        //  searchVal = item;
+          return item;
         }
-
+        return item;
       })
       console.log(this.result);
     }
